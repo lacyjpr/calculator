ea65a19a04c3a6a78d2546798e4bdbc7d52d87c9
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-02-26 14:14:00 -0800

    Fix media url

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
 	// Handle button clicks
 	$(".btn").on("click", function() {
-		new Audio("/media/click.mp3").play()
+		new Audio("/media/click1.mp3").play()
 		var val = ($(this).text());
 		switch (val) {
 		case "0":
